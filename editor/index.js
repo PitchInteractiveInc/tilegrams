488c32bc19fb3521d7edeebd3cae0bcce56d5abb
Adam Florin <adamflorin@gmail.com>
2016-08-26 13:38:56 -0700

    Use 110m US TopoJSON

--- editor/index.js
+++ editor/index.js
@@ -6,7 +6,7 @@ import Canvas from './source/Canvas'
 import {createElement} from './source/utils'
 
 // TEMP: load data from filesystem
-import usTopoJson from '../../../data/us-10m.json'
+import usTopoJson from '../../../data/us-110m.topo.json'
 
 const canvas = new Canvas(usTopoJson)
 
