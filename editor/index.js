d56649d7f3d776815972e418875f23822862d1e9
Adam Florin <adamflorin@gmail.com>
2016-08-26 17:36:57 -0700

    Use cartogram

--- editor/index.js
+++ editor/index.js
@@ -6,7 +6,7 @@ import Canvas from './source/Canvas'
 import {createElement} from './source/utils'
 
 // TEMP: load data from filesystem
-import usTopoJson from '../../../data/us-110m.topo.json'
+import usTopoJson from '../../../data/cartogram.geo.json'
 
 const canvas = new Canvas(usTopoJson)
 
