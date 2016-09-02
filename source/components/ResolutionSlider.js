4836beb72dc296d9389f6b72f0652f42c9b3de7b
Jessica Hamel <jessica@pitchinteractive.com>
2016-09-02 12:30:53 -0700

    reset scale when dataset is updated, clean scale to jump to rounded numbers

--- source/components/ResolutionSlider.js
+++ source/components/ResolutionSlider.js
@@ -6,59 +6,73 @@ export default class ResolutionSlider extends React.Component {
     super(props)
     this.state = {
       value: 50,
-      typedValue: null,
+      typedValue: '',
     }
-    this.normalizeValue = scaleLog().range([1, 100]).domain(this.props.metricDomain)
+    this.normalizeValue = scaleLog().domain(this.props.metricDomain).range([1, 99])
+    this._triggerChangeFromText = this._triggerChangeFromText.bind(this)
   }
 
   componentDidMount() {
-    this._onChange(50) // fire onChange on mount
+    this._triggerChange(50, {normalized: true}) // reset slider to 50
   }
 
   componentWillReceiveProps(nextProps) {
-    this.normalizeValue.domain(nextProps.metricDomain)
-    this.setState({typedValue: null})
+    if (JSON.stringify(nextProps) !== JSON.stringify(this.props)) {
+      this.normalizeValue.domain(nextProps.metricDomain) // update the domain
+      this._triggerChange(50, {normalized: true})
+    }
   }
 
-  _onChange(value) {
-    this.setState({value})
-    this.props.onChange(this.normalizeValue.invert(value))
+  _toSignificant(number) {
+    return parseFloat(number.toPrecision(2))
   }
 
-  _notifyChangeFromText() {
+  _triggerChange(value, options) {
+    /**
+     * changed can be triggered either
+     * 1) with a normalized value (in the range of this._normalizeValue) or
+     * 2) with a denormalized value (in the domain of this._normalizeValue)
+     * the first case is primarily for the slider
+     * the second case is for the typed box
+     * note: the value displayed in the input box is always the value passed
+     * to the props.onChange handler so that it is the source of truth
+     * for the hexagon metrics
+     */
+    options = options || {normalized: false}
+    if (options.normalized) {
+      const deNormalizedValue = this._toSignificant(this.normalizeValue.invert(value))
+      this.setState({value, typedValue: deNormalizedValue})
+      this.props.onChange(deNormalizedValue)
+    } else {
+      const normalizedValue = this._toSignificant(this.normalizeValue(value))
+      this.setState({value: normalizedValue, typedValue: value})
+      this.props.onChange(value)
+    }
+  }
+
+  _triggerChangeFromText() {
     const min = this.props.metricDomain[0]
     const max = this.props.metricDomain[1]
     if (this.state.typedValue < min) {
-      this.setState({
-        value: this.normalizeValue(min),
-        typedValue: min,
-      })
-      this.props.onChange(min)
+      this._triggerChange(this._toSignificant(min))
     } else if (this.state.typedValue > max) {
-      this.setState({
-        value: this.normalizeValue(max),
-        typedValue: max,
-      })
-      this.props.onChange(max)
+      this._triggerChange(this._toSignificant(max))
     } else {
-      const typedAsFloat = parseFloat(this.state.typedValue)
-      const typedToScaled = this.normalizeValue(typedAsFloat)
-      this.setState({value: typedToScaled})
-      this.props.onChange(typedAsFloat)
+      const typedAsFloat = this._toSignificant(parseFloat(this.state.typedValue))
+      this._triggerChange(typedAsFloat)
     }
   }
 
   _checkForEnter(event) {
     if (event.keyCode !== 13) return
-    this._notifyChangeFromText()
+    this.typedInput.blur()
   }
 
-  _onChangeText(typedValue) {
+  _setStateFromText(typedValue) {
     this.setState({typedValue})
   }
 
   render() {
-    const nPerTile = (this.normalizeValue.invert(this.state.value))
     return (
       <div>
         <fieldset>
@@ -67,18 +81,19 @@ export default class ResolutionSlider extends React.Component {
             id='resolutionSlider'
             type='range'
             min={1}
-            max={100}
-            onChange={(event) => this._onChange(event.target.value)}
+            max={99}
+            onChange={(event) => this._triggerChange(event.target.value, {normalized: true})}
             value={this.state.value}
           />
           <br />
           <br />
-          <label>Per tile:</label>
+          <label htmlFor='resolutionInput'>Per tile:</label>
           <input
+            ref={(ref) => { this.typedInput = ref }}
             type='text'
-            value={this.state.typedValue ? this.state.typedValue : nPerTile}
-            onChange={(event) => this._onChangeText(event.target.value)}
-            onBlur={this._notifyChangeFromText}
+            value={this.state.typedValue}
+            onChange={(event) => this._setStateFromText(event.target.value)}
+            onBlur={this._triggerChangeFromText}
             onKeyUp={(event) => this._checkForEnter(event)}
           />
         </fieldset>
