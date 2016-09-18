179c72419e33e95ca98665da982d83c9ca7b8231
Adam Florin <adamflorin@gmail.com>
2016-09-18 11:47:57 -0700

    Add more published tilegrams

--- source/TilegramData.js
+++ source/TilegramData.js
@@ -1,16 +1,22 @@
 import pitchPopulationTilegram from '../tilegrams/pitch-us-population-500k.json'
-import pitchGDPTilegram from '../tilegrams/gdp3.json'
+import nprOneToOneTilegram from '../tilegrams/npr-one-to-one.json'
+import fiveThirtyEightElectoralCollegeTilegram from
+  '../tilegrams/fivethirtyeight-electoral-college-tilegram.json'
 
 class TilegramData {
   constructor() {
     this._tilegrams = [
       {
-        label: 'Pitch GDP Population 2016',
-        topoJson: pitchGDPTilegram,
+        label: 'Pitch U.S. Population 2016 v1',
+        topoJson: pitchPopulationTilegram,
       },
       {
-        label: 'Pitch U.S. Population 2016',
-        topoJson: pitchPopulationTilegram,
+        label: 'FiveThirtyEight Electoral College 2016',
+        topoJson: fiveThirtyEightElectoralCollegeTilegram,
+      },
+      {
+        label: 'NPR 1-to-1',
+        topoJson: nprOneToOneTilegram,
       },
     ]
   }
