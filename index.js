117862ef7a6133dbabf2466718637252339fb33d
Adam Florin <adamflorin@gmail.com>
2016-10-18 15:54:05 -0700

    Ignore ctrl-Z altogether
    As Chrome does sneaky things to re-focus unfocused text inputs

--- index.js
+++ index.js
@@ -118,4 +118,11 @@ function resize() {
 window.onresize = resize
 resize()
 
+// Ignore ctrl-Z altogether
+document.addEventListener('keydown', event => {
+  if (e.metaKey && e.key === 'z') {
+    e.preventDefault()
+  }
+})
+
 init()
