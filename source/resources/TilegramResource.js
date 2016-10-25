38a859a9fb64c2e8efbcc6a4531c028dc97366cb
Adam Florin <adamflorin@gmail.com>
2016-10-25 16:54:22 -0700

    Add Pitch electoral college tilegram

--- source/resources/TilegramResource.js
+++ source/resources/TilegramResource.js
@@ -1,4 +1,5 @@
 import pitchPopulationTilegram from '../../tilegrams/pitch-us-population-500k.json'
+import pitchElectoralCollegeTilegram from '../../tilegrams/pitch-electoral-college.json'
 import nprOneToOneTilegram from '../../tilegrams/npr-one-to-one.json'
 import fiveThirtyEightElectoralCollegeTilegram from
   '../../tilegrams/fivethirtyeight-electoral-college-tilegram.json'
@@ -7,8 +8,12 @@ class TilegramResource {
   constructor() {
     this._tilegrams = [
       {
-        label: 'Pitch U.S. Population 2016 v1',
+        label: 'Pitch U.S. Population 2016',
         topoJson: pitchPopulationTilegram,
+      },
+      {
+        label: 'Pitch Electoral College',
+        topoJson: pitchElectoralCollegeTilegram,
       },
       {
         label: 'FiveThirtyEight Electoral College',
