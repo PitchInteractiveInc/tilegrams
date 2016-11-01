b0f1345bd6829f4fc45a192ef4f581f4f957bc3d
Adam Florin <adamflorin@gmail.com>
2016-11-01 16:50:28 -0700

    Add historic EC tilegrams with correct tilegramValues
    (but maps have wrong numbers of tiles)

--- source/resources/TilegramResource.js
+++ source/resources/TilegramResource.js
@@ -1,15 +1,25 @@
-import pitchElectoralCollegeTilegram from '../../tilegrams/pitch-electoral-college.json'
+import pitchElectoralCollege2012Tilegram from '../../tilegrams/pitch-electoral-college-2012.json'
+import pitchElectoralCollege2004Tilegram from '../../tilegrams/pitch-electoral-college-2004.json'
+import pitchElectoralCollege1992Tilegram from '../../tilegrams/pitch-electoral-college-1992.json'
 import pitchPopulationTilegram from '../../tilegrams/pitch-us-population-500k.json'
-import nprOneToOneTilegram from '../../tilegrams/npr-one-to-one.json'
 import fiveThirtyEightElectoralCollegeTilegram from
   '../../tilegrams/fivethirtyeight-electoral-college-tilegram.json'
+import nprOneToOneTilegram from '../../tilegrams/npr-one-to-one.json'
 
 class TilegramResource {
   constructor() {
     this._tilegrams = [
       {
-        label: 'Pitch Electoral College',
-        topoJson: pitchElectoralCollegeTilegram,
+        label: 'Pitch Electoral College 2012-2020',
+        topoJson: pitchElectoralCollege2012Tilegram,
+      },
+      {
+        label: 'Pitch Electoral College 2004-2008',
+        topoJson: pitchElectoralCollege2004Tilegram,
+      },
+      {
+        label: 'Pitch Electoral College 1992-2000',
+        topoJson: pitchElectoralCollege1992Tilegram,
       },
       {
         label: 'Pitch U.S. Population 2016',
